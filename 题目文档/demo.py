f458bd65af95bf3817d80d8fca560d4f03248e93
Martin_Xuc <xuchi36@h-partners.com>
2025-08-05 21:25:57 +0800

    修正并完善了 反转字符串 的代码实现

--- 题目文档/demo.py
+++ 题目文档/demo.py
@@ -1,9 +1,10 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s: list[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0, right = len(s) - 1
+        left = 0
+        right = len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left += 1
