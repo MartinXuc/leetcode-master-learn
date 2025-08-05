89a3833b5fa7534e556a07b00a4316c45d06df1c
Martin_Xuc <xuchi36@h-partners.com>
2025-08-05 22:18:53 +0800

    完成了 替换数字 的模拟 C++ 实现

--- 题目文档/demo.py
+++ 题目文档/demo.py
@@ -1,6 +1,27 @@
 s = input()
 s_list = list(s)
-for index, char in enumerate(s_list):
+number_count = 0
+for char in s:
     if ord(char) in range(ord('0'), ord('9') + 1):
-        s_list[index] = "number"
+        number_count += 1
+s_list.extend(['0'] * 5 * number_count)
+l = len(s) - 1
+r = len(s_list) - 1
+while l >= 0:
+    if ord(s_list[l]) in range(ord('0'), ord('9') + 1):
+        s_list[r] = 'r'
+        r -= 1
+        s_list[r] = 'e'
+        r -= 1
+        s_list[r] = 'b'
+        r -= 1
+        s_list[r] = 'm'
+        r -= 1
+        s_list[r] = 'u'
+        r -= 1
+        s_list[r] = 'n'
+    else:
+        s_list[r] = s_list[l]
+    l -= 1
+    r -= 1
 print("".join(s_list))
