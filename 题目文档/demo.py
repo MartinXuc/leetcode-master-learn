f145028e5fc640d838d25c304132fa56dd25b000
Martin_Xuc <xuchi36@h-partners.com>
2025-08-05 21:44:05 +0800

    完成了 反转字符串II 的初版实现

--- 题目文档/demo.py
+++ 题目文档/demo.py
@@ -1,16 +1,27 @@
 class Solution:
-    def reverseString(self, s: list[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        left = 0
-        right = len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+    def reverse_str_by_index(self, s: list, l: int, r: int) -> None:
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+    def reverseStr(self, s: str, k: int) -> str:
+        s_list = list(s)
+        i = 0 # 创建一个指针，用于遍历字符串
+        while True:
+            # 情况1，剩余字符数不足 k 个
+            if len(s_list) - i <= k:
+                self.reverse_str_by_index(s_list, i, len(s_list) - 1)
+                break
+            # 情况2，剩余字符数足 k 但不足 2k
+            if len(s_list) - i <= 2 * k:
+                self.reverse_str_by_index(s_list, i, i + k - 1)
+                break
+            # 其他情况
+            self.reverse_str_by_index(s_list, i, i + k - 1)
+            i += 2 * k
+        return "".join(s_list)
 
 if __name__ == "__main__":
-    s = ["h","e","l","l","o"]
-    Solution().reverseString(s)
-    print(s)
+    s = "abcdefg"
+    k = 2
+    print(Solution().reverseStr(s, k))
