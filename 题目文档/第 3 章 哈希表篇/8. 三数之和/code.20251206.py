5cfb125f6d83f2e0f68235308e985f564ed25b3c
MartinXuc <xuchicolson@163.com>
2025-12-06 11:44:44 +0800

    完成了一次练习：三数之和

---
+++ 题目文档/第 3 章 哈希表篇/8. 三数之和/code.20251206.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=15 lang=python3
+#
+# [15] 三数之和
+#
+
+# @lc code=start
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        ans = []
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k - 1] == nums[k]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+        return ans
+# @lc code=end
+
