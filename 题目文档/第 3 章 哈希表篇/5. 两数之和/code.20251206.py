ef1126eafa1080d62851cb3d5282c5629c96bfa5
MartinXuc <xuchicolson@163.com>
2025-12-06 10:32:14 +0800

    完成了一次练习：两数之和

---
+++ 题目文档/第 3 章 哈希表篇/5. 两数之和/code.20251206.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [i, seen[complement]]
+            else:
+                seen[num] = i
+        return None
+# @lc code=end
+
