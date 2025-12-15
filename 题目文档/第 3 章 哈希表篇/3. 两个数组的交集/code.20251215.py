b70b16c3f0b269f42d5fda074e8386636d944200
MartinXuc <xuchicolson@163.com>
2025-12-15 18:28:27 +0800

    完成了一次练习：两个数组的交集

---
+++ 题目文档/第 3 章 哈希表篇/3. 两个数组的交集/code.20251215.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=349 lang=python3
+#
+# [349] 两个数组的交集
+#
+
+# @lc code=start
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = []
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                if not ans or ans[-1] != nums1[i]:
+                    ans.append(nums1[i])
+                i += 1
+                j += 1
+        return ans
+# @lc code=end
+
